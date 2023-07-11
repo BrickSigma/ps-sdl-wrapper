87773d5ef13e175479a638e715d8e1c89d06b07e
paul-stone <stone.paul.green@gmail.com>
2023-07-11 15:42:17 +0300

    Types are defined in its own header

---
+++ src/types.h
@@ -0,0 +1,29 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+/* ================================================================ */
+/* =========================== WINDOW_T =========================== */
+/* ================================================================ */
+
+/**
+ * A structure that defines a window
+*/
+typedef struct _window* Window_t;
+
+/**
+ * A structure that defines a rendering context used by the window
+*/
+typedef struct _context* Context_t;
+
+/* ================================================================ */
+/* =========================== IMAGE_T ============================ */
+/* ================================================================ */
+
+/**
+ * A structure that defines an image for a surface context. CPU accelerated
+*/
+typedef struct SDL_Surface* Image_t;
+
+/* ================================================================ */
+
+#endif /* TYPES_H */
